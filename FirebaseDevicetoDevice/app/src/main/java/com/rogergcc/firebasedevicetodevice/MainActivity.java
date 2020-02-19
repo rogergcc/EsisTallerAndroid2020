cd325c226a3d5d179e39c59f2aaf3a7a6778c03a
Roger Colquehuanca Calcina <rogcolquehuancac@upt.pe>
2020-02-19 13:56:11 -0500

    crashlytics
    crashlytics red

--- FirebaseDevicetoDevice/app/src/main/java/com/rogergcc/firebasedevicetodevice/MainActivity.java
+++ FirebaseDevicetoDevice/app/src/main/java/com/rogergcc/firebasedevicetodevice/MainActivity.java
@@ -2,11 +2,13 @@ package com.rogergcc.firebasedevicetodevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -37,16 +39,20 @@ public class MainActivity extends AppCompatActivity {
     String TOPIC= "/topics/userABC";
     String SUBSCRIBE_TO = "userABC";
 
+    TextView tv_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtTitle = findViewById(R.id.edtTitle);
         edtMessage = findViewById(R.id.edtMessage);
-        Button btnSend = findViewById(R.id.btnSend);
+        tv_title = findViewById(R.id.tv_title);
+        final Button btnSend = findViewById(R.id.btnSend);
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 TOPIC = "/topics/userABC"; //topic has to match what the receiver subscribed to
                 NOTIFICATION_TITLE = edtTitle.getText().toString();
                 NOTIFICATION_MESSAGE = edtMessage.getText().toString();
@@ -61,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
                     Log.e(TAG, "onCreate: " + e.getMessage() );
                 }
                 sendNotification(notification);
+
+
+//                startActivity(new Intent(MainActivity.this,DetailsActivity.class));
             }
         });
         FirebaseInstanceId.getInstance().getInstanceId()
